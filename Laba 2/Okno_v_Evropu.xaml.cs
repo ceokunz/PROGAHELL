bb7db1d1991a5b59d1be68efc040b7562582a81f
yana261006 <yana261006@mail.ru>
2025-11-12 16:14:38 +0700

    ало

---
+++ Laba 2/Okno_v_Evropu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace laba_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Okno_v_Evropu.xaml
+    /// </summary>
+    public partial class Okno_v_Evropu : Window
+    {
+        public Okno_v_Evropu()
+        {
+            InitializeComponent();
+        }
+    }
+}
