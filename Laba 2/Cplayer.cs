b2a6b4efeb38ef173f1637caef0938dc0cf977d4
yana261006 <yana261006@mail.ru>
2025-11-18 16:23:01 +0700

    Допиписял

---
+++ Laba 2/Cplayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2
+{
+    public class Cplayer
+    {
+        private bool canClick;
+        private double timeBeforeClick;
+        private CCountdownTimer countdownTimer;
+
+        public Cplayer (double timeBeforeClick)
+        {
+
+        }
+
+        public void mouseClick (Point mousePosition)
+        {
+
+        }
+
+        public void countdownEnded()
+        {
+
+        }
+
+        public void update(double delta)
+        {
+
+        }
+
+        public void increaseSpeed(double speedModifier)
+        {
+
+        }
+    }
+}
