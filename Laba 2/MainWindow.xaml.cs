0d8fa74f96682257983d55b1c725979cb536bfc0
ceokunz <faablum@mail.ru>
2025-11-25 11:22:06 +0700

    \\\

--- Laba 2/MainWindow.xaml.cs
+++ Laba 2/MainWindow.xaml.cs
@@ -43,7 +43,7 @@ namespace laba_2
 
                 //new CEnemyTemplate("Valera", "20", "3", 80, "C:\\Users\\izzzz\\source\\repos\\ceokunz\\PROGAHELL\\laba 2\\monsters\\кунзик.png"),
                 //new CEnemyTemplate("Zlata", "20", "3", 80, "C:\\Users\\izzzz\\source\\repos\\ceokunz\\PROGAHELL\\laba 2\\monsters\\zlata.png"),
-                //new CEnemyTemplate("Sergey Alexeevich", "666", "10000000", 20, "C:\Users\user\Source\Repos\ceokunz\PROGAHELL\laba 2\monsters\alex.png"),
+                //new CEnemyTemplate("Sergey Alexeevich", "666", "10000000", 20, "C:\\Users\\user\\Source\\Repos\\ceokunz\\PROGAHELL\\laba 2\\monsters\\alex.png"),
                 //new CEnemyTemplate("Maxim Urich", "999", "10000000", 10, "C:\\Users\\izzzz\\source\\repos\\ceokunz\\PROGAHELL\\laba 2\\monsters\\max.png")
             };
 
