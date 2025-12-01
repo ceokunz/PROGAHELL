57f2bea819a0b9f3e8961e83a4c2184c762bd7ab
ceokunz <faablum@mail.ru>
2025-12-01 22:02:54 +0700

    Добатраченные темплеты

---
+++ Laba 2/EnemyTemplates/YkorachEnemyTempalate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2
+{
+    internal class YkorachEnemyTempalate : CEnemyTemplate
+    {
+        private double shrink;
+
+        public double Shrink
+        {
+            get { return shrink; }
+            set { if (value >= 0 && value <= 1.0) shrink = value; else shrink = 0.05; }
+        }
+        public YkorachEnemyTempalate(string name, string maxHitpoints, string goldReward, double spawnChance, string iconPath, double shrink) : base(name, maxHitpoints, goldReward, spawnChance, iconPath)
+        {
+            this.Shrink = shrink;
+        }
+    }
+}
